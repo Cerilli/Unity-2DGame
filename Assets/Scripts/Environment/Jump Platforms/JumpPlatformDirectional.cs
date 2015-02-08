95d16c07100b72f226915048483c48ec04f0d394
Cerilli <justincerilli@hotmail.com>
2015-02-08 01:52:29 -0500

    More work on Directional Jump Pads
    Got the math to work, still need to find a good way to actually display
    the trajectory though

--- Assets/Scripts/Environment/Jump Platforms/JumpPlatformDirectional.cs
+++ Assets/Scripts/Environment/Jump Platforms/JumpPlatformDirectional.cs
@@ -1,62 +1,87 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class JumpPlatformDirectional : MonoBehaviour
 {
 
 	public bool disableController;
-	//public float disableControllerTimer;
 	public AudioClip HitJumpPadSound;
-	public Transform jumpPadAim;
 	public GameObject MarkerFlag;
+	public float BounceMagnitude = 40f;
+	[Range (1, 179)]
+	public float BounceAngle = 45f;
 
-	private CharacterController2D _playerController;
+	public bool DEBUGshowArc = false;
+	private float _compareMagnitude;
+	private float _compareAngle;
+	private Vector2 _landingPosition;
+	private Vector2 _highestPoint;
+	private float _launchAngle; 
+
+	private LineRenderer _lineRenderer;
+
+	private Vector2 _qrtrTime;
+	private Vector2 _3qrtrTime;
+
+
+	void Awake()
+	{
+		_compareMagnitude = 0f;
+		_compareAngle = 0f;
+		_launchAngle = BounceAngle * Mathf.Deg2Rad;	
+
+		CalculateTrajectory(2.5f);
+	}
+
+	void Update()
+	{
+		if (DEBUGshowArc)
+		{			
+			// The idea is that I want to show the arc in the editor. Still WIP
+			if (BounceMagnitude != _compareMagnitude || BounceAngle != _compareAngle)
+			{
+				// NEED TO IMPLEMENT PLAYER GRAVITY, NOT STATIC NUMBER
+				CalculateTrajectory(2.5f);
+				_compareMagnitude = BounceMagnitude;
+				_compareAngle = BounceAngle;	
+			}
+
+			if(_lineRenderer == null)
+				_lineRenderer = GetComponent<LineRenderer>();
+
+			_lineRenderer.enabled = true;
+
+			// WILL USE LOOP AND ARRAY LATER, just figuring it out for now
+			_lineRenderer.SetVertexCount(5);
+			_lineRenderer.SetPosition(0, transform.position);
+			_lineRenderer.SetPosition(1, _qrtrTime);
+			_lineRenderer.SetPosition(2, _highestPoint);
+			_lineRenderer.SetPosition(3, _3qrtrTime);
+			_lineRenderer.SetPosition(4, _landingPosition);
+		}
+		if (!DEBUGshowArc && _lineRenderer != null)
+			_lineRenderer.enabled = false;
+	}
 
 	public void ControllerEnter2D(CharacterController2D controller)
 	{
-		// An attempt to make a jump pad which launches the player in/to a specified location. Still very WIP
-		// So far, if you place the jumpAim object directly above the jump pad (90 degree angle)
-		// the player will bounce exactly that high. Ultimately, I would like to have a gizmo that you can place in the scene
-		// which will be the landing point of the player when "launched", and some sort of visual representation of the trajectory
-		// in the scene view (not game view)
-
-
-		var finalHeight = (jumpPadAim.position.y - transform.position.y);
-		var grav = 2.5f;  //(controller.Parameters.generalMovement.Gravity * Time.deltaTime);
-
-		// Get the angle 
-		var launchAngle = Mathf.Atan2(finalHeight, (jumpPadAim.position.x - transform.position.x));
+		// Get the angle based on the JumppadAim transform);
+		var launchAngle = BounceAngle * Mathf.Deg2Rad;	
+		//var grav = (controller.Parameters.generalMovement.Gravity * Time.deltaTime);
 
 		// Used to determine the required magnitude on each axis
-		var scale_x = Mathf.Cos(launchAngle) ;
-		var scale_y = Mathf.Sin(launchAngle) ;
+		var XInitial = BounceMagnitude * Mathf.Cos(launchAngle) ;
+		var YInitial = BounceMagnitude * Mathf.Sin(launchAngle) ;
 
-		var magAdjust = grav*3.15f; // I don't know what I'm doing with math, but multiplaying the "magnitude" equation by this amount makes it work properly
-		var magnitude = Mathf.Sqrt(( 2* (grav * finalHeight) )) * magAdjust; // physics equation I looked up
-	
-		// SOLUTION? My gravity value is proprotionally approx. 1/4 of 9.81m/s, 
-		// so adjust magnitude to reflect that and not get massive numbers as a result
-		// I guess basically the magnitude and grav values aren't directly proportional
-		magAdjust = magnitude/4; 
-
-
-
-
-		// Find the landing distance
-		var sintheta = (2 * (Mathf.Sin(launchAngle) * Mathf.Cos(launchAngle)));
-		float landingDistance = ( Mathf.Pow(magAdjust,2) / grav) * sintheta;
-
-		//Debug.Log("Landing Dist: " + landingDistance);
-
-		var landingPosition = new Vector2( landingDistance, transform.position.y);
-
-		// Temporarily place a landing marker at the calculated destination. This is just for testing purposes
-		StartCoroutine(LandingMarker(landingPosition));
+		// Find total time in air
+		//var t = ((2 * YInitial) / grav) * Time.deltaTime;
 
 		if ( HitJumpPadSound != null)
 			AudioSource.PlayClipAtPoint(HitJumpPadSound, transform.position);
 
-		// Disable controller until we hit the ground, if desired.
+		// Disable controller until we hit the ground, if desired. It is switched back on
+		// in the Player script, when the player hits ground again
 		if(disableController)
 		{
 			controller.State.CanMoveFreely = false;
@@ -64,27 +89,32 @@ public class JumpPlatformDirectional : MonoBehaviour
 			controller.State.JumpPadDisabledControlsTimer = 0.02f;
 		}
 
-		controller.SetVerticalForce(magnitude * scale_y);
-		if(jumpPadAim.transform.position.x - transform.position.x < 0)
-			controller.SetHorizontalForce(magnitude*-1);
-		else 
-			controller.SetHorizontalForce(magnitude * scale_x);                                            
+		controller.SetForce(new Vector2(XInitial, YInitial));                                           
 	}
 
-	public void OnDrawGizmos()
+	private void CalculateTrajectory(float grav)
 	{
-		Debug.DrawLine(transform.position, jumpPadAim.position, Color.blue);
-	}
+		var launchAngle = BounceAngle * Mathf.Deg2Rad;	
+		var YInitial = BounceMagnitude * Mathf.Sin(launchAngle) ;
+		var XInitial = BounceMagnitude * Mathf.Cos(launchAngle) ;
+		var t = ((2 * YInitial) / grav) * Time.deltaTime;
 
-	public IEnumerator LandingMarker(Vector2 landingSpot)
-	{
-		if(MarkerFlag != null)
-		{
-			var flag = Instantiate(MarkerFlag, landingSpot, transform.rotation);
-			yield return new WaitForSeconds(5f);
-			Destroy(flag);
-		}
-	}
+		float landingDistance = (Mathf.Pow(BounceMagnitude,2) * Mathf.Sin(launchAngle*2) / grav) * Time.deltaTime;
+		float maxHeight = (Mathf.Pow (YInitial,2) / (2*grav)) * Time.deltaTime;
+				
+		// Temporarily place a landing marker at the calculated destination. This is just for testing purposes
+		var landingPosition = new Vector2( transform.position.x + landingDistance, transform.position.y);
+		var highestPoint = new Vector2((transform.position.x + landingDistance/2), maxHeight + transform.position.y );
 
+		// HACKY TEST METHOD =====
+		float qrtrTimeHeight = maxHeight * .75f;
+		_qrtrTime = new Vector2((transform.position.x + landingDistance*.25f), qrtrTimeHeight + transform.position.y);
+		_3qrtrTime = new Vector2((transform.position.x + landingDistance*.75f), qrtrTimeHeight + transform.position.y);
+		// =======================
+
+		_landingPosition = landingPosition;
+		_highestPoint = highestPoint;
+		_launchAngle = launchAngle;
+	}
 }
 
